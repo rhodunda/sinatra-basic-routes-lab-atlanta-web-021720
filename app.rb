dd7184525359b4fea39fd645ba2ac89f729d80da
Bryan Rhodunda <rhodunda@udel.edu>
2020-03-08 21:58:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Bryan Rhodunda"
+    end 
+    get '/hometown' do
+        "My hometown is Wilmington"
+    end 
+    get '/favorite-song' do
+        "My favorite song is Gorgeous"
+    end 
 end
